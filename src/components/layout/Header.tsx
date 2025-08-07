a0a03170ee6e2c1e2413dd84375bf9740e5501ab
lee0su <leeys4903@naver.com>
2025-08-07 16:33:16 +0900

    style: 진짜 개쩌는 CSS 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -64,7 +64,9 @@ function Header() {
                             <PopoverPanel
                                 transition
                                 className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 bg-transparent px-4 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
-                                style={{ transform: `translateX(calc(-50% + ${dropdownOffset}px))` }}
+                                style={{ transform: dropdownOffset === 0 
+                                    ? `translateX(0)` 
+                                    : `translateX(calc(-50% + ${dropdownOffset}px))` }}
                             >
                                 <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 shadow-lg ring-1 ring-gray-900/5">
                                     <div className="p-4">
